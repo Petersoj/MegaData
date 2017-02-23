468e61d600913749224b67c83dee5561c1f6397f
Petersoj <petersojj@gmail.com>
2017-02-23 13:06:26 -0700

    Fixed List.h

--- MegaData/Model/List.h
+++ MegaData/Model/List.h
@@ -9,29 +9,111 @@
 #ifndef Header_h
 #define Header_h
 
-#include <assert.h>
+#include "assert.h"
+#include "Node.hpp"
 
 template <class Type>
-class List {
-
+class List
+{
 private:
     int size;
+    Node<Type> * end;
     Node<Type> * front;
     
 public:
-    List<Type>();
-    List<Type>(const List<Type> & source);
+    List();
+    List(const List<Type> & source);
+    //Deconstructor
     ~List<Type>();
     
+    //Helper Methods
     void addAtIndex(int index, Type value);
-    void add(Type value);
-    Type remove(int index);
     Type setAtIndex(int index, Type data);
-    Type getFromIndex(int index, Type data);
-    bool contains(Type data);
+    Type getFromIndex(int index);
+    void addFront(Type value);
+    void addEnd(Type data);
+    Type remove(int index);
+    Type contains(Type data);
     int getSize() const;
-    Node<Type> * getFront() const;
+    Node<Type> * getFrontIndex() const;
+    Type getFrontIndex();
 };
+
+template <class Type>
+List<Type> :: List()
+{
+    this->size = 0;
+    this-> front = nullptr;
+    this-> end = nullptr;
+}
+
+template <class Type>
+void List<Type> :: addFront(Type value)
+{
+    if(size == 0)
+    {
+        Node<Type> * first = new Node<Type>(value);
+        this->front = first;
+        this->end = first;
+    }
+    else
+    {
+        Node<Type> * newFirst = new Node<Type>();
+        newFirst -> setNodeData(value);
+        newFirst -> setNodePointer(front);
+        this->front = newFirst;
+        
+    }
+    size++;
+}
+
+template <class Type>
+void List<Type> :: addEnd(Type data)
+{
+    Node<Type> * added = new Node<Type>(data);
+    if(size == 0)
+    {
+        this->front = added;
+        this->end = added;
+        
+    }
+    else
+    {
+        end->setNodePointer(added);
+        this->end = added;
+    }
+    size++;
+}
+
+template <class Type>
+void List<Type> :: addAtIndex(int index, Type value)
+{
+    assert(index >= 0 && index <= size);
+    if(index == 0)
+    {
+        addFront(value);
+    }
+    else if(index == size)
+    {
+        addEnd(value);
+    }
+    else
+    {
+        Node<Type> * insertNode = new Node<Type>(value);
+        Node<Type> * current = front;
+        Node<Type> * previous = nullptr;
+        
+        for(int position = 0; position < index; position++)
+        {
+            previous = current;
+            current = current->getNodePointer();
+        }
+        previous-> setNodePointer(insertNode);
+        insertNode-> setNodePointer(current);
+        
+        size++;
+    }
+}
 
 
 #endif /* Header_h */
