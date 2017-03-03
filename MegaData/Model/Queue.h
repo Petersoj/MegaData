55807d0e7c2c00cfc7f367a4935e99cc1d94cff2
Petersoj <petersojj@gmail.com>
2017-03-03 13:32:02 -0700

    Added Queue class

--- MegaData/Model/Queue.h
+++ MegaData/Model/Queue.h
@@ -9,5 +9,49 @@
 #ifndef Queue_h
 #define Queue_h
 
+template <class Type>
+class Queue : public DoublyLinkedList<Type>
+{
+private:
+public:
+    Queue();
+    ~Queue();
+    
+    void add(Type data);
+    Type remove(int index);
+    
+    void enqueue(Type data);
+    Type dequeue();
+    Type peek();
+};
 
-#endif /* Queue_h */
+template <class Type>
+void Queue<Type> :: add(Type value)
+{
+    add(value);
+}
+
+/*
+ Add to Queue:
+ Create Node
+ If First - adjust front
+ else add to end
+ move end
+ adjust size
+ */
+template <class Type>
+void Queue<Type> :: enqueue(Type instertedValue)
+{
+    BiDirectionalNode<Type> * added = BiDirectionalNode<Type>(insertedValue);
+    
+    if(this->getSize() == 0 || this->getFront == nullptr || this->getEnd() == nullptr)
+    {
+        this->Front(added);
+    }
+    else
+    {
+        this->getEnd()->setnextPointer(added);
+    }
+    this->setEnd(added);
+    this->setSize(this->getSize() + 1);
+}
