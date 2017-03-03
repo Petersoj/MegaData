56e97a8391a3fcd48ff5e7fa2667c56b46178241
Petersoj <petersojj@gmail.com>
2017-03-03 13:31:09 -0700

    Edited UML

---
+++ MegaData/Model/Queue.h
@@ -0,0 +1,13 @@
+//
+//  Queue.h
+//  MegaData
+//
+//  Created by Peterson, Jacob on 3/3/17.
+//  Copyright © 2017 Peterson, Jacob. All rights reserved.
+//
+
+#ifndef Queue_h
+#define Queue_h
+
+
+#endif /* Queue_h */
