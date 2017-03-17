305e04dbc758cdb2f7df631b530c24f3408b01fe
Petersoj <petersojj@gmail.com>
2017-03-17 13:07:29 -0600

    Fixed DoubleList

--- MegaData/Model/DoubleList.hpp
+++ MegaData/Model/DoubleList.hpp
@@ -71,17 +71,26 @@ Type DoubleList<Type> :: remove(int index)
 {
     Type derp;
     BiDirectionalNode<Type> * nodeToTakeOut = this->getFront();
-    for(int spot = 0; spot < index; spot++)
-    {
+    for(int spot = 0; spot < index; spot++){
         nodeToTakeOut = nodeToTakeOut->getNextPointer();
     }
     derp = nodeToTakeOut->getNodeData();
-    
-    BiDirectionalNode<Type> * prev = nodeToTakeOut->getPreviousPointer();
-    BiDirectionalNode<Type> * next = nodeToTakeOut->getNextPointer();
-    
-    prev->setNextPointer(next);
-    next->setPreviousPointer(prev);
+    if(this->getSize() > 1) {
+        BiDirectionalNode<Type> * prev = nodeToTakeOut->getPreviousPointer();
+        BiDirectionalNode<Type> * next = nodeToTakeOut->getNextPointer();
+        
+        prev->setNextPointer(prev);
+        next->setPreviousPointer(next);
+        
+        if(index == 0){
+            this->setFront(this->getFront()->getNextPointer());
+        }else if(index == this->getSize()-1){
+            this->setEnd(this->getEnd()->getPreviousPointer());
+        }
+    }else{
+        this->setFront(nullptr);
+        this->setEnd(nullptr);
+    }
     
     delete nodeToTakeOut;
     
