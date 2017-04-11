2cd77342a19a19900be6e0d43a791fe73e2b1f65
Petersoj <petersojj@gmail.com>
2017-04-11 13:55:22 -0600

    Added alternateIndexOf

--- MegaData/Model/DoubleList.hpp
+++ MegaData/Model/DoubleList.hpp
@@ -28,6 +28,7 @@ public:
     Type getFromIndexFast(int index);
     int indexOf(Type findMe);
     int nextIndexOf(Type value, int position);
+    int alternateIndexOf(Type findMe);
 };
 
 template <class Type>
@@ -188,6 +189,29 @@ int DoubleList<Type> :: nextIndexOf(Type value, int position)
     current = current->getNextPointer();
     
     return nextIndex;
+}
+
+template <class Type>
+int DoubleList<Type> :: alternateIndexOf(Type findMe)
+{
+    int alternate=0;
+    
+    for (BiDirectionalNode<Type> * search = this->getFront();
+         search != nullptr;
+         search = search->getNextPointer())
+    {
+        if (findMe != search->getNodeData())
+        {
+            alternate++;
+        }
+    }
+    if(alternate >= this->getSize())
+    {
+        return -1;
+    }else
+    {
+        return alternate;
+    }
 }
 
 #endif /* DoubleList_h */
